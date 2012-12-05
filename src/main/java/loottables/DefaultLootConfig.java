499649052660ac7ec93da596681dd4da9e5b9433
Jeremy Wood <farachan@gmail.com>
2012-12-05 09:37:14 -0500

    Moved FileUtil class into DefaultLootConfig class.

--- src/main/java/loottables/DefaultLootConfig.java
+++ src/main/java/loottables/DefaultLootConfig.java
@@ -9,7 +9,10 @@ import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.Plugin;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Map;
 import java.util.WeakHashMap;
 import java.util.logging.Logger;
@@ -101,5 +104,45 @@ class DefaultLootConfig implements LootConfig {
         cachedTables.put(name, newTable);
         log.fine("Loaded loot table from config.");
         return newTable;
+    }
+
+    public static class FileUtil {
+
+        /**
+         * Saves the sourceStream to the destFile.
+         */
+        public static void streamToFile(InputStream sourceStream, File destFile) throws IOException {
+            if (sourceStream == null) {
+                throw new IllegalArgumentException("sourceStream may not be null!");
+            }
+            if (destFile == null) {
+                throw new IllegalArgumentException("destFile may not be null!");
+            }
+            if(!destFile.exists()) {
+                destFile.createNewFile();
+            }
+            OutputStream out = null;
+            try {
+                out = new FileOutputStream(destFile);
+                int read = 0;
+                byte[] bytes = new byte[1024];
+                while ((read = sourceStream.read(bytes)) != -1) {
+                    out.write(bytes, 0, read);
+                }
+            } finally {
+                try {
+                    sourceStream.close();
+                } catch (IOException ignore) { }
+                if (out != null) {
+                    try {
+                        out.flush();
+                    } finally {
+                        try {
+                            out.close();
+                        } catch (IOException ignore) { }
+                    }
+                }
+            }
+        }
     }
 }
