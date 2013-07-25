95f2b7fbdfbbef202ffbeea7372997d7af33e411
Jeremy Wood <farachan@gmail.com>
2013-07-25 11:42:42 -0400

    initialize enchantments

--- editor/src/main/java/com/dumptruckman/minecraft/loottables/editor/Main.java
+++ editor/src/main/java/com/dumptruckman/minecraft/loottables/editor/Main.java
@@ -1,11 +1,15 @@
 package com.dumptruckman.minecraft.loottables.editor;
 
+import org.bukkit.enchantments.Enchantment;
+
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
+import java.lang.reflect.Field;
 
 public class Main {
 
     public static void main(String[] args) {
+        Enchantments.initializeEnchantments();
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
